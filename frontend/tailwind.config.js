94df521acc4ceee10d678b1f25fbb2f35d11185c
Lena05k <lena05k@yandex.ru>
2023-06-27 17:04:24 +0300

    Added screens

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile-s': [{'mim': '320px', 'max': '370px'}],
+        'mobile-l': [{'mim': '371px', 'max': '425px'}],
+        'mobile': [{'mim': '320px', 'max': '425px'}],
+        'tablet': [{'min': '425px', 'max': '768px'}],
+        'laptop': [{'min': '769px', 'max': '1199px'}],
+        'laptop-l': [{'min': '1200px', 'max': '1440px'}],
+        '4k': [{'min': '1441px'}],
+      },
+    },
   },
   plugins: [],
 }
